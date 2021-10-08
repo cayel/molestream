23c6eca2a2f962e75b0c758b758c88e16a8b77a3
Christophe Ayel <christophe.ayel@gmail.com>
2021-10-08 23:54:55 +0200

    test

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,8 @@ import pandas as pd
 import tools
 
 st.title('Molestream')
+st.write("gcp_service_account:", st.secrets["gcp_service_account"]["project_id"])
+'''
 df_gsheet = tools.spotifyGoogleSheet()
 df_artist= df_gsheet.groupby('artist').count()
 del df_artist['title']
@@ -10,3 +12,4 @@ df_artist = df_artist.sort_values(by=['played'], ascending=False)
 st.header('Top 10 listened artists')
 st.write('This top 10 is about listenings since the 22th of september 2021')
 st.table(df_artist.head(10))
+'''
