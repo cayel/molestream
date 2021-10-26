44bf34cb847a96a7417c6fa306b0be47464ae521
Christophe Ayel <christophe.ayel@gmail.com>
2021-10-26 22:03:52 +0200

    bar chart movies by year

--- movie.py
+++ movie.py
@@ -1,11 +1,18 @@
 # movie.py
 import streamlit as st
 import firebase_adapter
+import pandas as pd
 
 def load_data():
     return firebase_adapter.movie_collection_to_panda()
 
 def app():
     st.title('Movies')
-    data = load_data()
-    st.metric('Viewed',str(len(data.index)))
+    data = load_data()    
+    movie_cay = data[data['user'] == 'CKTlQvlnkJZhqagNIls2arTxKxp2' ]
+    st.metric('Viewed',str(len(movie_cay.index)))
+    movie_cay['year_watched'] = movie_cay['date'].dt.year
+    movies_by_year = movie_cay.groupby(pd.Grouper(key='year_watched')).count()
+    movies_by_year.rename(columns = {'cinema':'Count'}, inplace = True)
+    st.header('Watched by year')
+    st.bar_chart(movies_by_year[['Count']])
