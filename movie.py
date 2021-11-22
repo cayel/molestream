8ac291069f631b753d906f490c10784f63729a62
Christophe Ayel <christophe.ayel@gmail.com>
2021-11-22 21:50:53 +0100

    last watched movies

--- movie.py
+++ movie.py
@@ -12,7 +12,10 @@ def app():
     movie_cay = data[data['user'] == 'CKTlQvlnkJZhqagNIls2arTxKxp2' ]
     st.metric('Viewed',str(len(movie_cay.index)))
     movie_cay['year_watched'] = movie_cay['date'].dt.year
+    movie_cay = movie_cay.sort_values(by=['date'], ascending=False)
     movies_by_year = movie_cay.groupby(pd.Grouper(key='year_watched')).count()
     movies_by_year.rename(columns = {'cinema':'Count'}, inplace = True)
     st.header('Watched by year')
     st.bar_chart(movies_by_year[['Count']])
+    st.header('Last 10 watched movies')
+    st.table(movie_cay[["date","title", "director"]].head(10))
--- tools.py
+++ tools.py
@@ -26,3 +26,23 @@ def spotifyGoogleSheet() :
     df = pd.DataFrame(rows)
  
     return df
+
+def get_google_datasheet(sheet_url) :
+    # Create a connection object.
+    credentials = service_account.Credentials.from_service_account_info(
+        st.secrets["gcp_service_account"],
+        scopes=[
+            "https://www.googleapis.com/auth/spreadsheets",
+        ],
+    )
+    conn = connect(credentials=credentials)
+
+    # Perform SQL query on the Google Sheet.
+    # Uses st.cache to only rerun when the query changes or after 10 min.
+    # @st.cache(ttl=600)
+    def run_query(query):
+        rows = conn.execute(query, headers=1)
+        return rows
+
+    rows = run_query(f'SELECT played,title,artist FROM "{sheet_url}"')
+    return rows
